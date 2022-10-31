4464a8d36216a54ac5ca57afa4eac3537e807e7f
Sevrin94 <115183505+Sevrin94@users.noreply.github.com>
2022-10-31 11:12:09 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main extends JFrame implements ActionListener {
 
 
     JFrame pussel = new JFrame();
+    JButton blankTile = new JButton();
 
     public Main() {
         pussel.setBounds(0, 0, 500, 500);
@@ -17,20 +18,60 @@ public class Main extends JFrame implements ActionListener {
 
     public void knappar() {
         for (int i = 1; i < 16; i++) {
-            String j = String.valueOf(i);
-            pussel.add(new JButton(j));
-            (new JButton(j)).addActionListener(this);
-
+            String buttonLabel = String.valueOf(i);
+            JButton tile = new JButton(buttonLabel);
+            pussel.add(tile);
+            tile.addActionListener(this);
         }
+        pussel.add(blankTile);
+        blankTile.setVisible(false);
+        blankTile.addActionListener(this);
     }
 
 
     public void actionPerformed(ActionEvent e) {
-
+        JButton tile = (JButton) e.getSource();
+        int tileIndex = getIndexOfButton(tile);
+        if (
+              isValidMove(tileIndex)
+        ) {
+        blankTile.setText(tile.getText());
+        blankTile.setVisible(true);
+        tile.setVisible(false);
+        blankTile = tile; }
     }
 
+    public int getIndexOfButton(JButton tile) {
+        for (int i = 0; i < pussel.getContentPane().getComponents().length; i++) {
+            if (tile.hashCode() == pussel.getContentPane().getComponent(i).hashCode()) {
+                return i; }
 
-        public static void main(String[] args) {
-        new Main();
+            }
+        return -1;
+        }
+        public boolean isValidMove(int index) {
+            int RightIndex = index +1;
+            int LeftIndex = index -1;
+            int DownIndex = index +4;
+            int UpIndex = index -4;
+            if (UpIndex > -1 && pussel.getContentPane().getComponent(UpIndex) == blankTile) {
+                return true;
+            }
+            if (DownIndex < 16 && pussel.getContentPane().getComponent(DownIndex) == blankTile) {
+                return true;
+            }
+            if (LeftIndex !=-1 && LeftIndex !=3 && LeftIndex !=7 && LeftIndex !=11 && pussel.getContentPane().getComponent(LeftIndex) == blankTile) {
+                return true;
+            }
+            if (RightIndex !=4 && RightIndex !=8 && RightIndex !=12 && RightIndex !=16 && pussel.getContentPane().getComponent(RightIndex) == blankTile) {
+        return true;
+    }
+
+        return false;
+    }
+
+        public static void main (String[]args){
+            new Main();
         }
     }
+
